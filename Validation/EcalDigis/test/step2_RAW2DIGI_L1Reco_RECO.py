12f6c31cb906253b498cb103388d2f8220f4f120
Emilien Chapon <emilien.chapon@cern.ch>
2017-09-08 18:13:59 +0200

    Add modified files

--- Validation/EcalDigis/test/step2_RAW2DIGI_L1Reco_RECO.py
+++ Validation/EcalDigis/test/step2_RAW2DIGI_L1Reco_RECO.py
@@ -5,6 +5,7 @@
 # with command line options: step2 --process RECO --repacked --conditions 75X_dataRun2_PromptHI_v3 --scenario HeavyIons -s RAW2DIGI,L1Reco,RECO --datatier AOD --customise Configuration/DataProcessing/RecoTLR.customiseRun2CommonHI --data --eventcontent AOD --no_exec --filein /store/hidata/HIRun2015/HIMinimumBias1/RAW/v1/000/262/703/00000/D0A0C193-DB94-E511-B5C4-02163E011FB1.root
 import FWCore.ParameterSet.Config as cms
 
+<<<<<<< HEAD
 #######################################################
 # MASTER SWICTHes
 #######################################################
@@ -12,11 +13,20 @@ import FWCore.ParameterSet.Config as cms
 dataset = 'HIPhoton40AndZ'
 runEmulator = False
 emulScenario = 2016
+=======
+
+# MASTER SWICTH
+dataset = 'HIPhoton40AndZ'
+runEmulator = False
+emulScenario = 2016
+# dataset = 'HIEWQExo'
+>>>>>>> e0e59d06736... Add modified files
 trigger = '' # 'HLT_HIFullTrack12_L1Centrality010_v2' # 'HLT_L1MinimumBiasHF1OR_v1' # 'HLT_HIL1Tech7_NoBPTX_v1'
 # trigger = 'HLT_HIL1Centralityext50100MinimumumBiasHF2AND_v1'
 # HLTPaths = ['HLT_HIL1Centralityext50100MinimumumBiasHF2AND_v1'], # (300Hz, fed into MinimumBias1)
 # HLTPaths = ['HLT_HIL1MinimumBiasHF1ANDPixel_SingleTrack_v1'], #  (3Hz, fed into MinimumBias1)
 # HLTPaths = ['HLT_HIUCC020_v3'], # UCC 3Hz
+<<<<<<< HEAD
 nEvents = 300
 
 doReco = False       # do we want to get a reco file?
@@ -25,6 +35,8 @@ doFullDQM  = False       # do we want to get a detailed DQM file?
 doECALvalid = (not doReco) and (not doFullDQM)
 
 #######################################################
+=======
+>>>>>>> e0e59d06736... Add modified files
 
 process = cms.Process('RECO')
 process.options = cms.untracked.PSet( allowUnscheduled = cms.untracked.bool(True) )
@@ -41,6 +53,7 @@ process.load('Configuration.StandardSequences.L1Reco_cff')
 process.load('Configuration.StandardSequences.ReconstructionHeavyIons_cff')
 # process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('CommonTools.ParticleFlow.EITopPAG_cff')
+<<<<<<< HEAD
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 if doFullDQM:
    process.load('DQMOffline.Configuration.DQMOfflineHeavyIons_cff')
@@ -55,6 +68,13 @@ if doECALvalid:
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(nEvents)
+=======
+process.load('DQMOffline.Configuration.DQMOfflineHeavyIons_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(30)
+>>>>>>> e0e59d06736... Add modified files
 )
 
 # Input source
@@ -77,6 +97,7 @@ process.configurationMetadata = cms.untracked.PSet(
 
 # Output definition
 
+<<<<<<< HEAD
 if doReco:
    process.AODoutput = cms.OutputModule("PoolOutputModule",
        compressionAlgorithm = cms.untracked.string('LZMA'),
@@ -101,6 +122,29 @@ if doFullDQM:
        outputCommands = process.DQMEventContent.outputCommands,
        splitLevel = cms.untracked.int32(0)
    )
+=======
+process.AODoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('AOD'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    fileName = cms.untracked.string('step2_RAW2DIGI_L1Reco_RECO.root'),
+    outputCommands = process.AODEventContent.outputCommands
+)
+
+process.DQMoutput = cms.OutputModule("DQMRootOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('DQMIO'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('step2_RAW2DIGI_L1Reco_RECO_EI_DQM_inDQM.root'),
+    outputCommands = process.DQMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+>>>>>>> e0e59d06736... Add modified files
 
 # Additional output definition
 
@@ -114,11 +158,17 @@ process.L1Reco_step = cms.Path(process.L1Reco)
 process.reconstruction_step = cms.Path(process.reconstructionHeavyIons)
 # process.endjob_step = cms.EndPath(process.endOfProcess)
 process.eventinterpretaion_step = cms.Path(process.EIsequence)
+<<<<<<< HEAD
 if doFullDQM:
    process.dqmoffline_step = cms.Path(process.DQMOfflineHeavyIons)
    process.DQMoutput_step = cms.EndPath(process.DQMoutput)
 if doReco:
    process.AODoutput_step = cms.EndPath(process.AODoutput)
+=======
+process.dqmoffline_step = cms.Path(process.DQMOfflineHeavyIons)
+process.AODoutput_step = cms.EndPath(process.AODoutput)
+process.DQMoutput_step = cms.EndPath(process.DQMoutput)
+>>>>>>> e0e59d06736... Add modified files
 
 # initialize  MessageLogger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -137,6 +187,16 @@ process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 #process.load("Geometry.EcalMapping.EcalMapping_cfi")
 #process.load("Geometry.EcalMapping.EcalMappingRecord_cfi")
 
+<<<<<<< HEAD
+=======
+# DQM services
+# process.load("DQMServices.Core.DQM_cfg")
+# process.load("DQMServices.Components.DQMEnvironment_cfi")
+# process.dqmSaver.workflow = "/HIRun2015/Commissioning/RAW"
+# #process.load("CalibCalorimetry.Configuration.Ecal_FakeConditions_cff")
+# #process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+# process.load("DQM.Integration.config.FrontierCondition_GT_Offline_cfi")
+>>>>>>> e0e59d06736... Add modified files
 
 # Local database for emulator setup (to change ZS thresholds!)
 #----------------------------------------------------------------------
@@ -257,7 +317,11 @@ process.simEcalDigis.EESRPdigiCollection = cms.string('SReeDigis')#output Digis 
 #
 process.load("Validation.EcalDigis.ecalSelectiveReadoutValidation_cfi")
 process.ecalSelectiveReadoutValidation.outputFile = 'srvalid_hists.root'
+<<<<<<< HEAD
 process.ecalSelectiveReadoutValidation.verbose = cms.untracked.bool(True);
+=======
+process.ecalSelectiveReadoutValidation.verbose = cms.untracked.bool(False);
+>>>>>>> e0e59d06736... Add modified files
 
 #INPUTS:
 process.ecalSelectiveReadoutValidation.EbUnsuppressedDigiCollection = cms.InputTag("simEcalUnsuppressedDigis")
@@ -282,10 +346,13 @@ else:
    process.ecalSelectiveReadoutValidation.EeSrFlagCollection = cms.InputTag("simEcalDigis","eeSrFlags")#input
    process.ecalSelectiveReadoutValidation.TrigPrimCollection = cms.InputTag("simEcalDigis", "simEcalTriggerPrimitives")
    # process.ecalSelectiveReadoutValidation.TrigPrimCollection = cms.InputTag("ecalDigis", "EcalTriggerPrimitives")
+<<<<<<< HEAD
 
    from SimCalorimetry.EcalSimProducers.ecaldigi_cfi import *
    from SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi import *
 
+=======
+>>>>>>> e0e59d06736... Add modified files
    if emulScenario == 2016:
       process.ecalSelectiveReadoutValidation.ebZsThrADCCount = 4.5
       process.ecalSelectiveReadoutValidation.eeZsThrADCCount = 6.5
@@ -388,6 +455,7 @@ process.TriggerSelectionSequence = cms.Sequence(
       process.TriggerFilter
       )
 
+<<<<<<< HEAD
 if doECALvalid:
    process.p1 = cms.Path(
          process.ecalEBunpacker
@@ -401,6 +469,14 @@ else:
          # *process.simEcalTriggerPrimitiveDigis
          *process.simEcalDigis*process.ecalSelectiveReadoutValidation
          )
+=======
+process.p1 = cms.Path(
+      process.ecalEBunpacker
+      # *process.simEcalTriggerPrimitiveDigis
+      *process.simEcalDigis*process.ecalSelectiveReadoutValidation
+      # *process.dqmSaver
+      )
+>>>>>>> e0e59d06736... Add modified files
 
 # if trigger != '':
 #    process.p1 = cms.Path(
@@ -417,16 +493,30 @@ else:
 # to work you need (1) to uncomment the bypass flags below,
 # (2) you need to uncomment the 4 lines above (see "turn on the emulator" above)
 if runEmulator:
+<<<<<<< HEAD
    process.simEcalDigis.trigPrimBypass = cms.bool(True) # uncomment to bypass
    process.simEcalDigis.trigPrimBypassMode = cms.int32(1) #bypass mode (uncomment)
+=======
+   from SimCalorimetry.EcalSimProducers.ecaldigi_cfi import *
+   from SimCalorimetry.EcalSelectiveReadoutProducers.ecalDigis_cfi import *
+   # process.simEcalDigis.trigPrimBypass = cms.bool(True) # uncomment to bypass
+   # process.simEcalDigis.trigPrimBypassMode = cms.int32(1) #bypass mode (uncomment)
+>>>>>>> e0e59d06736... Add modified files
    # # process.simEcalDigis.trigPrimBypassLTH = cms.double(5.)# 2xGeV
    # # process.simEcalDigis.trigPrimBypassHTH = cms.double(11.)# 2xGeV
    #switch to apply selective readout decision on the digis and produce
    process.simEcalDigis.produceDigis = cms.untracked.bool(True)
+<<<<<<< HEAD
    # process.simEcalTriggerPrimitiveDigis.Label = cms.string('simEcalUnsuppressedDigis')
    # process.simEcalTriggerPrimitiveDigis.Label = cms.string('simEcalEBunpacker')
    # process.simEcalTriggerPrimitiveDigis.InstanceEB = cms.string('simEcalDigis')
    # process.simEcalTriggerPrimitiveDigis.InstanceEE = cms.string('simEcalDigis')
+=======
+   # process.simEcalTriggerPrimitiveDigis.Label = cms.string('ecalUnsuppressedDigis')
+   # process.simEcalTriggerPrimitiveDigis.Label = cms.string('ecalEBunpacker')
+   # process.simEcalTriggerPrimitiveDigis.InstanceEB = cms.string('ecalDigis')
+   # process.simEcalTriggerPrimitiveDigis.InstanceEE = cms.string('ecalDigis')
+>>>>>>> e0e59d06736... Add modified files
 
 # # customise for PbPb (not needed thanks to the --repacked option in the cmsDriver command
 # process.ecalEBunpacker.InputLabel = cms.InputTag('rawDataRepacker')
@@ -434,6 +524,7 @@ if runEmulator:
 ###############################################
 # Schedule definition
 # process.schedule = cms.Schedule(process.p1,process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,process.endjob_step,process.AODoutput_step)
+<<<<<<< HEAD
 process.schedule = cms.Schedule(process.p1
       # ,process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,
       # process.eventinterpretaion_step,
@@ -457,6 +548,13 @@ if doFullDQM:
 if doReco:
    process.schedule.append(process.AODoutput_step)
 
+=======
+process.schedule = cms.Schedule(process.p1,process.raw2digi_step,process.L1Reco_step,process.reconstruction_step,
+      # process.eventinterpretaion_step,
+      process.dqmoffline_step,process.AODoutput_step,process.DQMoutput_step)
+      # process.dqmoffline_step,process.DQMoutput_step)
+
+>>>>>>> e0e59d06736... Add modified files
 # customisation of the process.
 
 # Automatic addition of the customisation function from Configuration.DataProcessing.RecoTLR
@@ -559,6 +657,7 @@ def massReplaceInputTagUntracked(process,old="rawDataCollector",new="rawDataRepa
 ## END my mass replace
 #########################################################################
 
+<<<<<<< HEAD
 if runEmulator:
    if doFullDQM:
       massSearchReplaceAnyInputTagUntracked(process.dqmoffline_step,"ecalDigis","simEcalDigis")
@@ -580,6 +679,33 @@ if runEmulator:
    process.ecalDetIdToBeRecovered.ebSrFlagCollection = cms.InputTag("ecalDigis")
    process.ecalDetIdToBeRecovered.eeSrFlagCollection = cms.InputTag("ecalDigis")
 
+=======
+massSearchReplaceAnyInputTagUntracked(process.dqmoffline_step,"ecalDigis","simEcalDigis")
+massSearchReplaceAnyInputTagUntracked(process.reconstruction_step,"ecalDigis","simEcalDigis")
+massSearchReplaceAnyInputTagUntracked(process.dqmoffline_step,cms.InputTag("ecalDigis"),cms.InputTag("simEcalDigis"))
+massSearchReplaceAnyInputTagUntracked(process.reconstruction_step,cms.InputTag("ecalDigis"),cms.InputTag("simEcalDigis"))
+massSearchReplaceAnyInputTagUntracked(process.dqmoffline_step,cms.untracked.InputTag("ecalDigis"),cms.untracked.InputTag("simEcalDigis"))
+massSearchReplaceAnyInputTagUntracked(process.reconstruction_step,cms.untracked.InputTag("ecalDigis"),cms.untracked.InputTag("simEcalDigis"))
+for s in ["EcalIntegrityBlockSizeErrors", "EcalIntegrityChIdErrors", "EcalIntegrityGainErrors",
+"EcalIntegrityGainSwitchErrors", "EcalIntegrityMemBlockSizeErrors", "EcalIntegrityMemChIdErrors",
+"EcalIntegrityMemGainErrors", "EcalIntegrityMemTtIdErrors", "EcalIntegrityTTIdErrors",
+"EcalIntegrityTTIdErrors", "EcalTriggerPrimitives", "EcalTriggerPrimitives",
+"ebDigis", "eeDigis"]:
+   massSearchReplaceAnyInputTagUntracked(process.dqmoffline_step,cms.InputTag("ecalDigis",s),cms.InputTag("simEcalDigis",s))
+   massSearchReplaceAnyInputTagUntracked(process.reconstruction_step,cms.InputTag("ecalDigis",s),cms.InputTag("simEcalDigis",s))
+   massSearchReplaceAnyInputTagUntracked(process.dqmoffline_step,cms.untracked.InputTag("ecalDigis",s),cms.untracked.InputTag("simEcalDigis",s))
+   massSearchReplaceAnyInputTagUntracked(process.reconstruction_step,cms.untracked.InputTag("ecalDigis",s),cms.untracked.InputTag("simEcalDigis",s))
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+
+# feed the re-emulated ECAL digis into the downstream reco chain
+if runEmulator:
+>>>>>>> e0e59d06736... Add modified files
    # process.ecalCompactTrigPrim.inColl = cms.InputTag("simEcalTriggerPrimitiveDigis","simEcalTriggerPrimitives")
    process.ecalCompactTrigPrim.inColl = cms.InputTag("ecalDigis","EcalTriggerPrimitives")
    process.ecalMultiFitUncalibRecHit.EBdigiCollection = cms.InputTag("simEcalDigis","SRebDigis")
@@ -589,12 +715,15 @@ if runEmulator:
    process.ecalRecHit.triggerPrimitiveDigiCollection = cms.InputTag("ecalDigis","EcalTriggerPrimitives")
    process.ecalTPSkim.tpInputCollection = cms.InputTag("ecalDigis","EcalTriggerPrimitives")
 
+<<<<<<< HEAD
 # Automatic addition of the customisation function from Configuration.DataProcessing.Utils
 from Configuration.DataProcessing.Utils import addMonitoring 
 
 #call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
 process = addMonitoring(process)
 
+=======
+>>>>>>> e0e59d06736... Add modified files
 if trigger != '':
    # filter all path with the production filter sequence
    process.superFilterPath = cms.Path(process.TriggerFilter)
